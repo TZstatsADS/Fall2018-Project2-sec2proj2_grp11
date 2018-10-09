d85556c9c57aa0a70d1baae7a5f20c45e7d5343d
ouyangfangqi <41164650+ouyangfangqi@users.noreply.github.com>
2018-10-09 12:41:19 -0400

    Add files via upload

--- app/server.R
+++ app/server.R
@@ -1,169 +1,119 @@
-# install.packages(c("choroplethr", "choroplethrZip", "leaflet", "maps", "ragal"))
-library(shiny)
-library(choroplethr)
-library(choroplethrZip)
-library(dplyr)
+#install.packages("lubridate")
+library(ggmap)
+library(maptools)
+library(qmap)
+library(plotly)
 library(leaflet)
-library(maps)
+library(dplyr)
+library(shiny)
+library(reshape2)
+library(shinydashboard)
+library(plyr)
+library(ggplot2)
+library(flexdashboard)
 library(rgdal)
+library(lubridate)
+library(geojsonio)
 
-## Define Manhattan's neighborhood
-man.nbhd=c("all neighborhoods", "Central Harlem", 
-           "Chelsea and Clinton",
-           "East Harlem", 
-           "Gramercy Park and Murray Hill",
-           "Greenwich Village and Soho", 
-           "Lower Manhattan",
-           "Lower East Side", 
-           "Upper East Side", 
-           "Upper West Side",
-           "Inwood and Washington Heights")
-zip.nbhd=as.list(1:length(man.nbhd))
-zip.nbhd[[1]]=as.character(c(10026, 10027, 10030, 10037, 10039))
-zip.nbhd[[2]]=as.character(c(10001, 10011, 10018, 10019, 10020))
-zip.nbhd[[3]]=as.character(c(10036, 10029, 10035))
-zip.nbhd[[4]]=as.character(c(10010, 10016, 10017, 10022))
-zip.nbhd[[5]]=as.character(c(10012, 10013, 10014))
-zip.nbhd[[6]]=as.character(c(10004, 10005, 10006, 10007, 10038, 10280))
-zip.nbhd[[7]]=as.character(c(10002, 10003, 10009))
-zip.nbhd[[8]]=as.character(c(10021, 10028, 10044, 10065, 10075, 10128))
-zip.nbhd[[9]]=as.character(c(10023, 10024, 10025))
-zip.nbhd[[10]]=as.character(c(10031, 10032, 10033, 10034, 10040))
+##########Load all the RData file 
+#shapefile <- readShapePoly('MyApp/data/nynta_shapefile/nynta.shp')
+setwd("../")
+load('output/meanFarePerDistance.RData')
+load('output/meanTipsPerDistance.RData')
+load('output/MeanTipPerDistanceBorough.RData')
+load('output/MeanFarePerDistanceBorough.RData')
+rownames(FPD_final) <- c("Manhattan","Brooklyn","Queens","Bronx","Staten Island")
+rownames(TPD_final) <-c("Manhattan","Brooklyn","Queens","Bronx","Staten Island")
+boroughs <- readOGR(dsn="data/BB.geojson")
+boroughs <- spTransform(boroughs,CRS("+init=epsg:4326"))
 
-## Load housing data
-load("../output/count.RData")
-load("../output/mh2009use.RData")
+bounds <- bbox(boroughs)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
   
-  ## Neighborhood name
-  output$text = renderText({"Selected:"})
-  output$text1 = renderText({
-      paste("{ ", man.nbhd[as.numeric(input$nbhd)+1], " }")
-  })
-  
-  ## Panel 1: summary plots of time trends, 
-  ##          unit price and full price of sales. 
-  
-  output$distPlot <- renderPlot({
-    
-    ## First filter data for selected neighborhood
-    mh2009.sel=mh2009.use
-    if(input$nbhd>0){
-      mh2009.sel=mh2009.use%>%
-                  filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
+ fare_data_borough <- reactive({
+    if(!is.null(input$Month)){
+      nyc_fare_borough <- data.frame(FPD=FPD_final[,input$Month],TPD=TPD_final[,input$Month],boro_name=c("Manhattan","Brooklyn","Queens","Bronx","Staten Island"))
+
     }
-    
-    ## Monthly counts
-    month.v=as.vector(table(mh2009.sel$sale.month))
-    
-    ## Price: unit (per sq. ft.) and full
-    type.price=data.frame(bldg.type=c("10", "13", "25", "28"))
-    type.price.sel=mh2009.sel%>%
-                group_by(bldg.type)%>%
-                summarise(
-                  price.mean=mean(sale.price, na.rm=T),
-                  price.median=median(sale.price, na.rm=T),
-                  unit.mean=mean(unit.price, na.rm=T),
-                  unit.median=median(unit.price, na.rm=T),
-                  sale.n=n()
-                )
-    type.price=left_join(type.price, type.price.sel, by="bldg.type")
-    
-    ## Making the plots
-    layout(matrix(c(1,1,1,1,2,2,3,3,2,2,3,3), 3, 4, byrow=T))
-    par(cex.axis=1.3, cex.lab=1.5, 
-        font.axis=2, font.lab=2, col.axis="dark gray", bty="n")
-    
-    ### Sales monthly counts
-    plot(1:12, month.v, xlab="Months", ylab="Total sales", 
-         type="b", pch=21, col="black", bg="red", 
-         cex=2, lwd=2, ylim=c(0, max(month.v,na.rm=T)*1.05))
-    
-    ### Price per square foot
-    plot(c(0, max(type.price[,c(4,5)], na.rm=T)), 
-         c(0,5), 
-         xlab="Price per square foot", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                  type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$unit.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$unit.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$unit.mean, 1:nrow(type.price), 
-              type.price$unit.median, 1:nrow(type.price),
-             lwd=2)    
-    
-    ### full price
-    plot(c(0, max(type.price[,-1], na.rm=T)), 
-         c(0,5), 
-         xlab="Sales Price", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                   type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$price.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$price.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$price.mean, 1:nrow(type.price), 
-             type.price$price.median, 1:nrow(type.price),
-             lwd=2)    
-  })
-  
-  ## Panel 2: map of sales distribution
-  output$distPlot1 <- renderPlot({
-    count.df.sel=count.df
-    if(input$nbhd>0){
-      count.df.sel=count.df%>%
-        filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
+    else{
+      nyc_fare_borough <- data.frame(FPD=FPD_final[,6],TPD=TPD_final[,6],boro_name=c("Manhattan","Brooklyn","Queens","Bronx","Staten Island")) 
     }
-    # make the map for selected neighhoods
-    
-    zip_choropleth(count.df.sel,
-                   title       = "2009 Manhattan housing sales",
-                   legend      = "Number of sales",
-                   county_zoom = 36061)
+   joinedDataset <- boroughs
+   joinedDataset@data <- suppressWarnings(left_join(joinedDataset@data, nyc_fare_borough, by="boro_name"))
+   
+   #boroughs<-SpatialPolygonsDataFrame(boroughs, data=boroughs1)
+   return(joinedDataset)
   })
-  
-  ## Panel 3: leaflet
-  output$map <- renderLeaflet({
-    count.df.sel=count.df
-    if(input$nbhd>0){
-      count.df.sel=count.df%>%
-        filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
+ 
+ ############Can't find the bug.........
+ #output$nycmap<-renderLeaflet({
+   #leaflet() %>%
+    # addTiles() %>%
+     
+     # Centre the map in the middle of our co-ordinates
+     #setView(mean(bounds[1,]),
+            # mean(bounds[2,]),
+             #zoom=5 # set to 10 as 9 is a bit too zoomed out
+     #)       
+   
+ #})
+   
+  #observe({
+   #theData<-fare_data_borough()
+   #pal <- colorQuantile("YlGn", theData$FPD, n = 10)
+   #text <- as.character(month(ymd(010101) + months(input$Month-1),label=TRUE,abbr=FALSE))
+    #set text for the clickable popup labels
+   #borough_popup <- paste0("<strong>Borough: </strong>", 
+                           #theData$boro_name, 
+                          #FO "<br><strong>",
+                           #text," 
+                           #Fare Per Distance: </strong>", theData$FPD
+                           
+   #)
+   
+   
+   #leafletProxy("nycmap",theData) %>%
+   # clearShapes() %>%
+   # addPolygons(
+                # fillColor = theData$FPD, 
+                # fillOpacity = 1, 
+                 #color = "#BDBDC3", 
+                #weight = 2,
+               # popup = borough_popup)  
+   
+ #})
+ 
+ 
+output$maxbox <- renderInfoBox(
+    {
+      DF <- fare_data_borough()
+      outData <- DF$FPD
+      max_value <- max(outData,na.rm = T)
+      max_neigh <- DF$boro_name[outData==max_value]
+      infoBox(max_neigh,round(max_value,2),icon = icon("chevron-up"),color='yellow')
     }
-    
-    # From https://data.cityofnewyork.us/Business/Zip-Code-Boundaries/i8iw-xf4u/data
-    NYCzipcodes <- readOGR("../data/ZIP_CODE_040114.shp",
-                           #layer = "ZIP_CODE", 
-                           verbose = FALSE)
-    
-    selZip <- subset(NYCzipcodes, NYCzipcodes$ZIPCODE %in% count.df.sel$region)
-    
-    # ----- Transform to EPSG 4326 - WGS84 (required)
-    subdat<-spTransform(selZip, CRS("+init=epsg:4326"))
-    
-    # ----- save the data slot
-    subdat_data=subdat@data[,c("ZIPCODE", "POPULATION")]
-    subdat.rownames=rownames(subdat_data)
-    subdat_data=
-      subdat_data%>%left_join(count.df, by=c("ZIPCODE" = "region"))
-    rownames(subdat_data)=subdat.rownames
-    
-    # ----- to write to geojson we need a SpatialPolygonsDataFrame
-    subdat<-SpatialPolygonsDataFrame(subdat, data=subdat_data)
-    
-    # ----- set uo color pallette https://rstudio.github.io/leaflet/colors.html
-    # Create a continuous palette function
-    pal <- colorNumeric(
-      palette = "Blues",
-      domain = subdat$POPULATION
-    )
-    
-    leaflet(subdat) %>%
-      addTiles()%>%
-      addPolygons(
-        stroke = T, weight=1,
-        fillOpacity = 0.6,
-        color = ~pal(POPULATION)
-      )
-  })
+  )
+  output$minbox <- renderInfoBox(
+    {
+      DF <- fare_data_borough()
+      outData <- DF$FPD
+      min_value <- min(outData,na.rm = T)
+      min_neigh <- DF$boro_name[outData==min_value]
+      infoBox(min_neigh,round(min_value,2),icon = icon("chevron-down"),color='blue')
+    }
+  )
+  output$medbox <- renderInfoBox(
+    {
+      DF <- fare_data_borough()
+      outData <- DF$FPD
+      mean_value <- mean(outData,na.rm = T)
+      mean_neigh <- DF$boro_name[outData==mean_value]
+      text <- as.character(month(ymd(010101) + months(input$Month-1),label=TRUE,abbr=FALSE))
+      infoBox(paste("Mean Fare for", text),round(mean_value,2),icon = icon("calculator"),color='red')
+    }
+  )
+  
 })
+
+
